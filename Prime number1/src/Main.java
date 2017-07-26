ba39beea7685f407f52cf5008d20a2358ac018e1
David jiang <David jiang>
2017-07-26 17:28:13 +0100

    week3 update2

--- Prime number1/src/Main.java
+++ Prime number1/src/Main.java
@@ -1,29 +1,37 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
-		 
-        public static void main(String[] args) {
-               
-                //define limit
-                int limit = 3000000;
-               
-                System.out.println("Prime numbers between 1 and " + limit);
-               
-                //loop through the numbers one by one
-                for(int i=1000000; i < limit; i++){
-                       
-                        boolean isPrime = true;
-                       
-                        //check to see if the number is prime
-                        for(int j=2; j < i ; j++){
-                               
-                                if(i % j == 0){
-                                        isPrime = false;
-                                        break;
-                                }
-                        }
-                        // print the number
-                        if(isPrime)
-                                System.out.println(i + " ");
-                }
-        }
+
+	public static void main(String[] args) {
+
+		// define limit
+		int limit = 3000000;
+		int count = 0;
+
+		System.out.println("Prime numbers between 1 and " + limit);
+
+		// loop through the numbers one by one
+		for (int i = 1; i < limit; i++) {
+
+			boolean isPrime = true;
+			count++;
+			// check to see if the number is prime
+			for (int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			// print the number
+			if (isPrime) 
+			{
+				System.out.println(i + " ");
+			}
+		}
+
+	}
 }
